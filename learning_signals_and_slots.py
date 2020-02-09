1ff05a9253b6a0749ccbe43f14b4aefc3e68bfad
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-02-09 17:24:30 +0000

    added up and down buttons with lcd

--- learning_signals_and_slots.py
+++ learning_signals_and_slots.py
@@ -2,21 +2,42 @@ import sys
 from PySide2 import QtWidgets, QtCore, QtGui
 
 
+class LCDCounter(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+
+        # LCD display
+        lcd = QtWidgets.QLCDNumber(2)
+
+        # Increment buttons
+        button_up = QtWidgets.QPushButton("+1")
+        button_down = QtWidgets.QPushButton("-1")
+
+        # Set layout
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(lcd)
+        layout.addWidget(button_up)
+        layout.addWidget(button_down)
+        self.setLayout(layout)
+
+
 class MyWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
 
-        quit = QtWidgets.QPushButton("Quit")
+        # Quit button
+        quit = QtWidgets.QPushButton("&Quit")
         quit.setFont(QtGui.QFont("Times", 18, QtGui.QFont.Bold))
+
+        # LCD counter
+        lcd_counter = LCDCounter()
 
         self.connect(quit, QtCore.SIGNAL("clicked()"),
                      QtWidgets.qApp, QtCore.SLOT("quit()"))
 
-        grid = QtWidgets.QGridLayout()
-
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(quit)
-        layout.addLayout(grid)
+        layout.addWidget(lcd_counter)
         self.setLayout(layout)
 
 
